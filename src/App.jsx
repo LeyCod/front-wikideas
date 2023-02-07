d0228a777dd531de12661bde7104d8349bc8f509
Leandro Sabus <leandro.sabus@gmail.com>
2023-02-07 17:39:26 -0300

    theme added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import { useState } from 'react'
 export const App = () => {
 
   return (
-    <h1>Wiki<span>TOT</span></h1>
+    <>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-title text-titleSize'>Wiki<span className='text-alternative'>TOT</span></h1>
+        <h4 className='text-subtitle text-subtitleSize'>Ejemplo de subtitulo</h4>
+        <span className='text-cards font-cardsSize'>Titulo de card</span>
+        <p className='font-normalText'>Ejemplo de texto normal</p>
+        <button className='bg-buttons rounded p-2'>Ejemplo</button>
+      </div>
+    </>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,22 @@ module.exports = {
   ],
   theme: {
     colors: {
-      "blue": "#eo44ef",
+      'title': '#1e2022',
+      'subtitle': '#677788',
+      'cards': '#001b4d',
+      'bg-footer': '#001b4d',
+      'buttons': '#377dff',
+      'alternative': '#ea5e60',
     },
+    fontFamily: {
+      lato: ['Lato', 'sans-serif'],
+    },
+    fontSize: {
+      titleSize: '46px',
+      subtitleSize: '24px',
+      cardsSize: '21px',
+      normalText: '16px'
+    }
   },
   plugins: [],
 }
