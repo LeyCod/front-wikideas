33cb1c99b2ca61bead928cebaf35ec52f8af50ae
Leandro Sabus <leandro.sabus@gmail.com>
2023-02-13 18:57:16 -0300

    routes text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Card = () => {
     return (
-        <div className='text-red-500'>Card</div>
+        <div className='text-red-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo exercitationem saepe excepturi, quod, et totam, voluptas delectus enim quaerat quia impedit. Ducimus incidunt unde nobis aliquid reiciendis impedit fugit saepe.</div>
     )
 }
--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const CreateArticle = () => {
     return (
-        <h1>CreateArticle</h1>
+
+        <h1 >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, optio corrupti labore, velit doloribus quos incidunt recusandae inventore quisquam veritatis omnis, placeat sed? Culpa blanditiis ea deserunt ipsam, est corporis.</h1>
+
+
     )
 }
