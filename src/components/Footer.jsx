099e4c79afd0dfcfb21e567d8f9b838697d58af5
Antonella Duca <antonelladuca@gmail.com>
2023-03-13 18:08:42 -0300

    responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,29 @@ import { AiOutlineInstagram, AiFillLinkedin, AiOutlineTwitter, AiOutlineGithub} 
 export const Footer = () => {
 
     return (
-        <footer className='flex flex-row justify-center items-center bg-bg-footer text-selected h-24'>
+        <footer className="flex flex-col md:flex-row justify-center items-center bg-bg-footer text-selected md:h-24 p-5">
             {/* Links footer */}
             <Link to="/" className="mr-5">Políticas de privacidad</Link>
-            <p> / </p>
-            <Link to="/" className="mr-5 ml-5">Términos y condiciones</Link>
-            <p> / </p>
-            <Link to="/" className="ml-5 mr-10">Sobre nosotros</Link>
+            <span className="hidden sm:flex"> / </span>
+            <Link to="/" className="mr-5 ml-5 mt-2 md:mt-0">Términos y condiciones</Link>
+            <span className="hidden sm:flex"> / </span>
+            <Link to="/" className="ml-5 mr-10 mt-2 md:mt-0">Sobre nosotros</Link>
 
             {/* iconos footer */}
-            <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
-                <AiOutlineInstagram className="text-selected text-cardsSize"/> 
-            </Link>
-            <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
-                <AiFillLinkedin className="text-selected text-cardsSize"/>
-            </Link>
-            <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
-                <AiOutlineTwitter className="text-selected text-cardsSize"/>
-            </Link>
-            <Link className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded">
-                <AiOutlineGithub className="text-selected text-cardsSize"/>
-            </Link>
+            <div className='flex mt-2 md:mt-0'>
+                <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
+                    <AiOutlineInstagram className="text-selected text-cardsSize"/> 
+                </Link>
+                <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
+                    <AiFillLinkedin className="text-selected text-cardsSize"/>
+                </Link>
+                <Link to="/" className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded mr-2">
+                    <AiOutlineTwitter className="text-selected text-cardsSize"/>
+                </Link>
+                <Link className="h-8 w-8 bg-alternative flex flex-row justify-center items-center rounded">
+                    <AiOutlineGithub className="text-selected text-cardsSize"/>
+                </Link>
+            </div>
         </footer>
     )
 }
