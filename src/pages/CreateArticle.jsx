833fb2cefb755753b150fdb59679e2540ac26211
Leandro Sabus <leandro.sabus@gmail.com>
2023-04-17 22:13:50 -0300

    create article logic

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,50 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiBold, BiItalic, BiUnderline, BiStrikethrough, BiListUl } from 'react-icons/bi'
 import { HiCodeBracket } from 'react-icons/hi2'
 // import { HiOutlineCodeBracket } from 'react-icons/hi'
 import { RiDoubleQuotesL } from 'react-icons/ri'
 
 export const CreateArticle = () => {
+    const [formData, setFormData] = useState({ title: '', content: '' })
 
-    // const makeBoldText = (text) => {
-    //     return <b>{text}</b>
-    // }
 
+    const handleFormChange = event => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        fetch(`https://tot.kame-code.com/api/entries`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+
+            .then(response => response.json())
+            .catch(error => console.error(error));
+    };
+    console.log(formData)
 
     return (
         <div className='flex flex-col w-full p-4 justify-center items-start '>
             <h1 className='text-subtitleSize' >Editar Artículo</h1>
+            <form onSubmit={handleSubmit} >
+                <h3 className='text-titleSize mt-6'>Título</h3>
+                <div className='flex flex-row  items-center md:items-start  w-full '>
+                    <input
+                        name='title'
+                        id='title'
+                        value={formData.title}
+                        onChange={handleFormChange}
+                        className='rounded shadow-md mt-2 shadow-title w-full md:w-[100%] h-10 p-2' type="text" placeholder="Título del artículo" ></input>
 
-            <h3 className='text-titleSize mt-6'>Título</h3>
-            <div className='flex flex-row  items-center md:items-start  w-full '>
-                <input
-                    className='rounded shadow-md mt-2 shadow-title w-full md:w-[100%] h-10 p-2' type="text" placeholder="Título del artículo" ></input>
-
-            </div>
-
-            <div className='flex flex-col mt-6'>
-                <h3 className='text-subtitleSize'>Contenido</h3>
-                <div className='flex flex-row gap-4 items-center  text-subtitle mt-4'>
-                    <BiBold size={18} />
-                    <BiItalic size={18} />
-                    <BiUnderline size={18} />
-                    <BiStrikethrough size={18} />
-                    <BiListUl size={18} />
-                    <RiDoubleQuotesL size={18} />
-                    <HiCodeBracket size={18} />
                 </div>
-            </div>
 
-            <div className='h-auto flex items-start w-[800px] mt-6'>
-                <span>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe repellendus, iusto incidunt dicta sunt quos repudiandae illum nesciunt culpa modi, earum deleniti? Nostrum facilis itaque libero at et odio sint.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe repellendus, iusto incidunt dicta sunt quos repudiandae illum nesciunt culpa modi, earum deleniti? Nostrum facilis itaque libero at et odio sint.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe repellendus, iusto incidunt dicta sunt quos repudiandae illum nesciunt culpa modi, earum deleniti? Nostrum facilis itaque libero at et odio sint.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe repellendus, iusto incidunt dicta sunt quos repudiandae illum nesciunt culpa modi, earum deleniti? Nostrum facilis itaque libero at et odio sint.
-                </span>
+                <div className='flex flex-col mt-6'>
+                    <h3 className='text-subtitleSize'>Contenido</h3>
+                    <div className='flex flex-row gap-4 items-center  text-subtitle mt-4'>
+                        <BiBold size={18} />
+                        <BiItalic size={18} />
+                        <BiUnderline size={18} />
+                        <BiStrikethrough size={18} />
+                        <BiListUl size={18} />
+                        <RiDoubleQuotesL size={18} />
+                        <HiCodeBracket size={18} />
+                    </div>
+                </div>
 
-            </div>
-            <button className='bg-buttons text-selected p-1 rounded mt-4'>Crear Artículo</button>
+                <div className='h-auto flex items-start w-[800px] mt-6'>
+                    <textarea
+                        name="content"
+                        id="content"
+                        value={formData.content}
+                        onChange={handleFormChange}
+                        cols="80"
+                        rows="10"></textarea>
 
+                </div>
+                <button
+                    type='submit'
+                    className='bg-buttons text-selected p-1 rounded mt-4'>Crear Artículo</button>
+            </form>
         </div>
     )
 }
--- src/pages/EditArticle.jsx
+++ src/pages/EditArticle.jsx
@@ -94,29 +94,3 @@ export const EditArticle = () => {
     )
 }
 
-    // <div key={id} className=' max-w-[1140px] md:w-[800px]  p-4  '>
-    //     <h1 className='text-subtitleSize md:text-titleSize'>Editar Artículo</h1>
-
-    //     <h4 className='text-subtitleSize font-bold mt-4'>Título</h4>
-    //     <span className='  mt-4 text-subtitle shadow-xl p-2'>{data.title}</span>
-
-    //     <h4 className='text-subtitleSize font-bold mt-10'>Contenido</h4>
-    //     <div className='flex flex-row gap-4 items-center p-4 md:p-0  text-subtitle mt-4'>
-    //         <BiBold size={18} />
-    //         <BiItalic size={18} />
-    //         <BiUnderline size={18} />
-    //         <BiStrikethrough size={18} />
-    //         <BiListUl size={18} />
-    //         <RiDoubleQuotesL size={18} />
-    //         <HiCodeBracket size={18} />
-    //     </div>
-
-    //     <div className='shadow-xl  text-start mt-4'>
-    //         <p>{data.content}</p>
-    //     </div>
-
-    //     <div className='flex justify-center md:justify-end mt-6 '>
-    //         <button className='bg-buttons text-selected rounded p-1'>Editar Artículo</button>
-    //     </div>
-
-    // </div>
